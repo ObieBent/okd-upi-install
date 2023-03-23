466057303fe6a2889dfa4bbbfc88fd1044882205
Boris ASSOGBA <borisassogba@live.fr>
2023-03-24 00:06:38 +0100

    function blocks

--- deployBastion.sh
+++ deployBastion.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
+set -e -u -o pipefail
+
+_log() {
+    local level=$1; shift
+    echo -e "$level: $@"
+}
+
+info() {
+    _log "\nINFO" "$@"
+}
 
 # Disk creation
-qemu-img create -o preallocation=metadata -f qcow2 /var/lib/libvirt/pool/ssd/bastion.eazytraining.lab.qcow2 200G
+bastion.disk_creation() {
+    info "Creating disk"
+    qemu-img create -o preallocation=metadata -f qcow2 /var/lib/libvirt/pool/ssd/bastion.eazytraining.lab.qcow2 200G
+    sleep 15
+}
 
-sleep 15
 
 # Bastion Installation
- virt-install \
- --network network:ocpnet \
- --name testing \
- --os-type=linux \
- --location /var/lib/libvirt/pool/ssd/iso/AlmaLinux-8.7-x86_64-minimal.iso \
- --ram=8192 \
- --os-variant=almalinux8 \
- --vcpus=4 \
- --disk /var/lib/libvirt/pool/ssd/bastion.eazytraining.lab.qcow2 --boot hd,menu=on\
- --nographics \
- --initrd-inject /ks.cfg \
- --extra-args "inst.ks=file:/ks.cfg console=tty0 console=ttyS0,115200n8"
+bastion.installation() {
+    info "Deploying Alma Linux 8.7 on the server"
+    virt-install \
+    --network network:ocpnet \
+    --name testing \
+    --os-type=linux \
+    --location /var/lib/libvirt/pool/ssd/iso/AlmaLinux-8.7-x86_64-minimal.iso \
+    --ram=8192 \
+    --os-variant=almalinux8 \
+    --vcpus=4 \
+    --disk /var/lib/libvirt/pool/ssd/bastion.eazytraining.lab.qcow2 --boot hd,menu=on\
+   --nographics \
+    --initrd-inject /ks.cfg \
+    --extra-args "inst.ks=file:/ks.cfg console=tty0 console=ttyS0,115200n8"
+}
+
+bastion.disk_creation
+bastion.installation
